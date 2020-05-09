8606f92b8e2bd66efc8e4340ab4658227841042b
bys562bai@gmail.com <bys562bai@gmail.com>
2020-05-09 19:09:41 -0400

    0

--- src/include/cubui/framework/baseinterface.h
+++ src/include/cubui/framework/baseinterface.h
@@ -15,20 +15,11 @@ struct IMsgHandler : BaseType{
     virtual ~IMsgHandler(){}
     virtual bool support_msg(MsgID id){return false;}
     protected:
-    virtual void when(IMsgHandler* prnt, IMsgHandler* sender, Message* msg){}
+    virtual void when(Message* msg, IMsgHandler* prnt, IMsgHandler* sender){}
 };
-
-
-struct ISceneNode : IMsgHandler{
-    //不带引用计数, unique
-    virtual IScene* prnt() = 0;
-    virtual void destroy() = 0;
-};
-
-struct SceneNodeIterator;
 
 struct IScene : IMsgHandler{
-    //自带引用计数, shared
+    //应当以shared_ptr方式保存
     virtual Result init(Renderer& renderer) = 0;
     // virtual void init(Renderer& renderer, bool multi_renderer = false) = 0;
     virtual Result reinit(Renderer& renderer) = 0;
@@ -37,29 +28,24 @@ struct IScene : IMsgHandler{
     virtual void uninit() = 0;
     virtual Renderer& get_renderer() = 0;
 
-    virtual void draw(Renderer& renderer) = 0;
+    virtual void draw(RenderOutput& out) = 0;
     // virtual void draw(Renderer& renderer, RenderOutput& out) = 0;
-    virtual bool has_son() = 0;
-    virtual SceneNodeIterator sons_begin() = 0;
-    virtual SceneNodeIterator sons_end() = 0;
 };
 
 //desc, info, config
-struct Scene2DNodeInfo{
+struct Object2DInfo{
     //边界位置，高度
     //输入边界是否为0，是否无界
     //输出边界是否为0，是否无界
 };
 
-struct IScene2DNode : ISceneNode{
-    virtual void getObject2DInfo(Scene2DNodeInfo& info) = 0;
+struct IObject2D {
+    virtual void getObject2DInfo(Object2DInfo& info) = 0;
 };
+
+
 
 struct IScene2D : IScene{
-
-};
-
-struct IScene3DNode : ISceneNode{
 
 };
 
